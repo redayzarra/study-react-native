648c82f9db23f0c25da6cb73b056f951ed094f4b
ReDay Zarra <redayzarra@gmail.com>
2023-06-05 05:44:26 -0400

    Learning how to log statements for React Native and debugging

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed"); // Logging statements
+
   return (
     <View style={styles.container}>
       <Text>Hello React Native!</Text>
