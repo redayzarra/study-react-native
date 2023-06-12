f61a8edfe1f693a1cadfcbb3f6216091ddbbf2c7
ReDay Zarra <redayzarra@gmail.com>
2023-06-12 05:15:17 -0400

    Understanding touchables and their types

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, Image, SafeAreaView, TouchableHighlight } from 'react-native';
 
 export default function App() {
   let x = 1;
@@ -9,15 +9,16 @@ export default function App() {
       <Text numberOfLines={1} onPress={handlePress}>
         My React Native Project
       </Text>
-
-      <Image 
-      blurRadius={0}
-      fadeDuration={1000}
-      source={{ 
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300" 
-      }}/>
+      <TouchableHighlight onPress={() => console.log("Image clicked!")}>
+        <Image 
+        blurRadius={0}
+        fadeDuration={1000}
+        source={{ 
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300" 
+        }}/>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
