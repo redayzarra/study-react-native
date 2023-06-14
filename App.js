3d56ec48af178882e90c0ee590432453e80a675b
ReDay Zarra <redayzarra@gmail.com>
2023-06-14 05:10:20 -0400

    Learning about flex directions and its properties

--- App.js
+++ App.js
@@ -8,18 +8,25 @@ export default function App() {
     <View style={{
       backgroundColor: "#fff",
       flex: 1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
     }}>
       <View style={{
         backgroundColor: "dodgerblue",
-        flex: 2,
+        width: 100, 
+        height: 300,
+        alignSelf: "flex-start",
       }} />
       <View style={{
         backgroundColor: "gold",
-        flex: 1,
+        width: 100, 
+        height: 200,
       }} />
       <View style={{
         backgroundColor: "tomato",
-        flex: 1,
+        width: 100, 
+        height: 100,
       }} />
     </View>
   );
