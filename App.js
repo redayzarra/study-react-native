0bd77beaa28bfa089af24bfeac1569d4e9a98a9d
ReDay Zarra <redayzarra@gmail.com>
2023-06-16 05:32:15 -0400

    Starting view image screen for items

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   )
 }
