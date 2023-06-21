a6af0988d1887582f1b46a14ba8dd2a40c6e28dd
ReDay Zarra <redayzarra@gmail.com>
2023-06-21 05:24:05 -0400

    Understanding padding and margin

--- App.js
+++ App.js
@@ -14,11 +14,25 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          shadowColor: "gray",
-          shadowOffset: { width: 0, height: 10 },
-          shadowOpacity: 1,
-          shadowRadius: 10,
-          elevation: 20,
+          padding: 20,
+          paddingHorizontal: 10,
+          paddingLeft: 30,
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}
+        ></View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+          margin: 20,
         }}
       ></View>
     </View>
