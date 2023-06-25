8f140aca4a13060991bee504784183319216dcdc
ReDay Zarra <redayzarra@gmail.com>
2023-06-25 05:27:24 -0400

    Building listing details screen

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
