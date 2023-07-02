e4237e38b332d1449f058e7098ec24ca4df96fd7
ReDay Zarra <redayzarra@gmail.com>
2023-07-02 09:28:27 -0400

    Making the list item component reusable

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+      <ListItem
+        title="My title"
+        subTitle="My subtitle"
+        ImageComponent={<Icon name="email" />}
+      />
     </Screen>
   );
 }
