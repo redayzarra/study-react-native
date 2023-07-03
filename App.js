137bcfbeb54277884a09fb3613d1513cfd7f194f
ReDay Zarra <redayzarra@gmail.com>
2023-07-03 09:26:06 -0400

    Building account screen

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return (
-    <Screen>
-      <ListItem
-        title="My title"
-        subTitle="My subtitle"
-        ImageComponent={<Icon name="email" />}
-      />
-    </Screen>
-  );
+  return <AccountScreen />;
 }
---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import { FlatList, StyleSheet } from "react-native";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+
+const menuItems = [
+  {
+    title: "My listings",
+    icon: {
+      name: "format-list-bullet",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <ListItem
+          title="ReDay"
+          subTitle="myemail@gmail.com"
+          image={require("../assets/mosh.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              ImageComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+});
+
+export default AccountScreen;
