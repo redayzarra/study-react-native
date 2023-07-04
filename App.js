16d4ce05c9e917d40eddf74b8095b1026dee6228
ReDay Zarra <redayzarra@gmail.com>
2023-07-04 05:31:45 -0400

    Building frontend for ListingsScreen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <AccountScreen />;
+  return <ListingsScreen />
 }
