4c7b281921844f878a52d48f7df5347083eb08c0
ReDay Zarra <redayzarra@gmail.com>
2023-07-07 05:33:24 -0400

    Learning how to handle picker selections for custom component

--- app/components/PickerItem.js
+++ app/components/PickerItem.js
@@ -2,8 +2,12 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import AppText from "./AppText";
 import { StyleSheet } from "react-native";
+import { useState } from "react";
 
 function PickerItem({ label, onPress }) {
+
+  const [category, setCategory] = useState();
+
   return (
     <TouchableOpacity onPress={onPress}>
       <AppText style={styles.text}>{label}</AppText>
