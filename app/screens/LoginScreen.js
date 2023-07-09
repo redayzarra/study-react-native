c9889663a95ea66f022276bde173051a7efc1aeb
ReDay Zarra <redayzarra@gmail.com>
2023-07-08 21:43:55 -0400

    Starting login screen

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+function LoginScreen(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  
+})
