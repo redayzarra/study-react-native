6748c896a16b492ad9b9cea4244aeb9c4ea542f2
ReDay Zarra <redayzarra@gmail.com>
2023-07-08 22:05:43 -0400

    Wrapping up front end elements for login screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { Image, StyleSheet } from "react-native";
+import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
 
 function LoginScreen(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
-    <div>
-      
-    </div>
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="email"
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+        placeholder="Email"
+        textContentType="emailAddress"
+      />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="lock"
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Password"
+        secureTextEntry
+        textContentType="password"
+      />
+      <AppButton title="Login" onPress={() => console.log(email, password)} />
+    </Screen>
   );
 }
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  
-})
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
