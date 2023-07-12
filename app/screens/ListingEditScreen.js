d5693b32655e161f45f4ac7742314732a78f0435
ReDay Zarra <redayzarra@gmail.com>
2023-07-12 19:30:28 -0400

    Adding validation schema for listing edit screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,18 +1,22 @@
 import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
 import Screen from "../components/Screen";
-import { AppForm, AppFormField, AppFormPicker, SubmitButton } from "../components/forms";
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from "../components/forms";
 
 const validationSchema = Yup.object.shape({
-
-})
+  title: Yup.string().required().min(1).label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
+  description: Yup.string().label("Description"),
+  category: Yup.object().required().nullable().label("Category"),
+});
 
 function ListingEditScreen(props) {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 }
 
 export default ListingEditScreen;
