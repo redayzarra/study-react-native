dcbbe2a7d6b29e495658fa423492c5cc688284ba
ReDay Zarra <redayzarra@gmail.com>
2023-07-13 05:23:19 -0400

    Truncating large texts for list items

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -19,8 +19,14 @@ function ListItem({
           {IconComponent}
           {image && <Image source={image} style={styles.image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText style={styles.title} numberOfLines={1}>
+              {title}
+            </AppText>
+            {subTitle && (
+              <AppText style={styles.subTitle} numberOfLines={2}>
+                {subTitle}
+              </AppText>
+            )}
           </View>
           <MaterialCommunityIcons
             name="chevron-right"
