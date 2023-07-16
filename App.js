b4c94dced27e4aa335e578d12e77570dda602aa8
ReDay Zarra <redayzarra@gmail.com>
2023-07-16 17:41:04 -0400

    Using the ImagePicker library to allow users to select and image

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
-import Screen from "./app/components/Screen";
 import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
+import { useEffect, useState } from "react";
+import { Button, Image } from "react-native";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  const requestPermission = async () => {
-    const result = await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.LOCATION_BACKGROUND)
-    result.granted
+  const [imageUri, setImageUri] = useState();
 
+  const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert("You need to grant camera roll permissions!");
   };
@@ -15,5 +14,20 @@ export default function App() {
   useEffect(() => {
     requestPermission();
   }, []);
-  return <Screen></Screen>;
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image!");
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
