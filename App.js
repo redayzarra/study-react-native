e8640bf8fee4557d9c8f0f605a61f0ebfc6acd24
ReDay Zarra <redayzarra@gmail.com>
2023-07-16 17:32:15 -0400

    Exploring expo's permissios module for requesting user permissions

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 import Screen from "./app/components/Screen";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export default function App() {
   const requestPermission = async () => {
+    const result = await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.LOCATION_BACKGROUND)
+    result.granted
+
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert("You need to grant camera roll permissions!");
   };
