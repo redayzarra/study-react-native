6ad5df96e44f19f3d15c44d700014a9d3a973bb5
redayzarra <redayzarra@gmail.com>
2023-07-19 22:05:26 -0400

    Finishing image input list for multiple images

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -6,20 +6,23 @@ function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
   return (
     <View style={styles.container}>
       {imageUris.map((uri) => (
-        <ImageInput
-          imageUri={uri}
-          onChangeImage={() => onRemoveImage(uri)}
-          key={uri}
-        />
+        <View key={uri} style={styles.image}>
+          <ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+        </View>
       ))}
 
       <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
   },
+  image: {
+    marginRight: 10,
+  },
 });
+
 export default ImageInputList;
