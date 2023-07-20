2cfb922d0ba7e04ed58721bfe02de46f4974553c
redayzarra <redayzarra@gmail.com>
2023-07-19 21:41:04 -0400

    Building image input component with async

--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,16 +1,35 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 import colors from "../config/colors";
+import * as ImagePicker from "expo-image-picker";
 
 function ImageInput({ imageUri }) {
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image!");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons color={colors.medium} name="camera" size={40} />
-      )}
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="camera"
+            size={40}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
