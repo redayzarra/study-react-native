49034cf69366d81f221ff29af105e92a94385ff4
redayzarra <redayzarra@gmail.com>
2023-07-24 18:54:09 -0400

    Changing the titlel of screens dynamically

--- App.js
+++ App.js
@@ -30,7 +30,11 @@ const Stack = createStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator initialRouteName="Tweets">
     <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({ route }) => ({ title: route.params.id })}
+    />
   </Stack.Navigator>
 );
 
