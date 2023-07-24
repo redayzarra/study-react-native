ce6f641ac8b45c801ac4b09b67c406884f07cbb9
redayzarra <redayzarra@gmail.com>
2023-07-23 20:09:52 -0400

    Installing stack navigator and creating tweets

--- App.js
+++ App.js
@@ -1,4 +1,18 @@
+import { Text } from "react-native";
+import Screen from "./app/components/Screen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+)
 
 export default function App() {
   return <ListingEditScreen />;
