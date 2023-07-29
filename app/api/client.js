0c5dd9ed5e5bb82dc76a2c03fa69bd4c6413a8a2
ReDay Zarra <redayzarra@gmail.com>
2023-07-29 05:23:40 -0400

    Creating an api layer to fetch listings

--- app/api/client.js
+++ app/api/client.js
@@ -1,11 +1,7 @@
 import { create } from "apisauce";
 
-const apiClient = create({
+const client = create({
   baseURL: "http://108.20.21.75:9000/api",
 });
 
-apiClient.get("/listings").then((response) => {
-  if (!response.ok) {
-    response.problem;
-  }
-});
+export default client;
